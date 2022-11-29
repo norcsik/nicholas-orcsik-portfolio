60c75e5f20adc449c0352f698282033667b627ef
Nicholas Orcsik <w0541685@myscc.ca>
2022-11-28 23:20:53 -0500

    #13, #12 Styled the work cards and displayed cards by twos

--- src/helper-files/work-db.ts
+++ src/helper-files/work-db.ts
@@ -5,7 +5,7 @@ let work1: Work;
 work1 = {
     id: 0,
     name: "BookNook",
-    date: "November 28, 2022",
+    date: "Sep - Dec 2022",
     blurb: "A book searching and collection app for Apple devices.",
     description: "Proper paragraph talking about the project.", 
     img: "https://returntofreedom.org/store/wp-content/uploads/default-placeholder.png",
@@ -16,12 +16,23 @@ let work2: Work;
 work2 = {
     id: 1,
     name: "MealMate",
-    date: "April 25, 2022",
+    date: "Feb - Apr 2022",
     blurb: "A recipe searching app for Android devices.",
     description: "Proper paragraph talking about the project.", 
     img: "https://returntofreedom.org/store/wp-content/uploads/default-placeholder.png",
     tags: ["Java", "Android", "API", "SQLite"]
 };
 
+let work3: Work;
+work3 = {
+    id: 2,
+    name: "Tic Tac Tome",
+    date: "Mar - Apr 2021",
+    blurb: "A tic tac toe game written in JavaFX.",
+    description: "Proper paragraph talking about the project.", 
+    img: "https://returntofreedom.org/store/wp-content/uploads/default-placeholder.png",
+    tags: ["Java", "IntelliJ", "Game"]
+};
+
 //Array of works
-export let ALLWORKS: Work[] = [work1, work2];;
+export let ALLWORKS: Work[] = [work1, work2, work3];;
