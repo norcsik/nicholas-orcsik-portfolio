119bccc7c566e6c8b1de8b1f93a88765fc20b7c7
Nicholas Orcsik <w0541685@myscc.ca>
2022-11-30 05:11:15 -0500

    #19 Added and styled content in contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { WorkDetailComponent } from './components/work-detail/work-detail.compon
 import { HttpClientModule } from "@angular/common/http";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from "./services/in-memory-data.service";
-import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
